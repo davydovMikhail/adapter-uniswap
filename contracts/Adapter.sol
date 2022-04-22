2ad569fb15cfc47800056cae11959ef1e017769e
davydovMikhail <davydov27977@gmail.com>
2022-04-22 12:13:10 +0700

    contract2

--- contracts/Adapter.sol
+++ contracts/Adapter.sol
@@ -1,10 +1,14 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-// import "hardhat/console.sol";
+
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Adapter {
+    using SafeERC20 for IERC20;
+
     address router;
     address public WETH;
 
@@ -26,7 +30,10 @@ contract Adapter {
             uint256 liquidity
         )
     {
-        // UniswapV2Library.pairFor(factory, _tokenA, _tokenB);
+        IERC20(_tokenA).safeTransferFrom(msg.sender, address(this), _amountA);
+        IERC20(_tokenB).safeTransferFrom(msg.sender, address(this), _amountB);
+        IERC20(_tokenA).safeApprove(router, _amountA);
+        IERC20(_tokenB).safeApprove(router, _amountB);
         (amountA, amountB, liquidity) = IUniswapV2Router02(router).addLiquidity(
             _tokenA,
             _tokenB,
@@ -42,10 +49,13 @@ contract Adapter {
     function removeLiquidity(
         address _tokenA,
         address _tokenB,
+        address _lpToken,
         uint256 _liquidity,
         uint256 _amountA,
-        uint256 _amountB,
+        uint256 _amountB
     ) external returns (uint256 amountA, uint256 amountB) {
+        IERC20(_lpToken).safeTransferFrom(msg.sender, address(this), _liquidity);
+        IERC20(_lpToken).safeApprove(router, _liquidity);
         (amountA, amountB) = IUniswapV2Router02(router).removeLiquidity(
             _tokenA,
             _tokenB,
@@ -66,12 +76,14 @@ contract Adapter {
     }
 
     function exhangesTokens(uint256 _in, uint256 _minOut, address[] calldata _path) external returns (uint[] memory amounts) {
+        IERC20(_path[0]).safeTransferFrom(msg.sender, address(this), _in);
+        IERC20(_path[0]).safeApprove(router, _in);
         amounts = IUniswapV2Router02(router).swapExactTokensForTokens(
             _in,
             _minOut,
             _path,
             msg.sender,
             block.timestamp + 30
-        )
+        );
     }
 }
